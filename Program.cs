6e52b24a34340d982c6fe69d728eac7c3686e7f8
NATA <armida780@mail.ru>
2023-02-18 21:49:40 +0300

    квадрат_IF

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Clear();
+Console.Write("Введите число A: ");
+int NumberA = int.Parse(Console.ReadLine());
+Console.Write("Введите число B: ");
+int NumberB = int.Parse(Console.ReadLine());
+int sqr = Convert.ToInt32 (Math.Pow(NumberA, 2));
+if (sqr == NumberB)
+{
+    Console.WriteLine(value: $"Квадрат числа A ({NumberA}) равен числу ({NumberB}) и это Верно");
+}
+else {Console.WriteLine("не верно");}
+
+
